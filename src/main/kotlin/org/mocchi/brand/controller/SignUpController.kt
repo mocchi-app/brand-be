45dd612a50e14fa6914937bc101ca9cefcbe74da
sausageRoll <drozdovigor1996@gmail.com>
2020-06-16 00:39:11 +0200

    Fix url mapping

--- src/main/kotlin/org/mocchi/brand/controller/SignUpController.kt
+++ src/main/kotlin/org/mocchi/brand/controller/SignUpController.kt
@@ -4,9 +4,11 @@ import org.mocchi.brand.model.controller.SignUpDto
 import org.mocchi.brand.service.SignUpService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/v1/brand")
+@RestController
+@RequestMapping("/api/v1/brand")
 class SignUpController(
     private val signUpService: SignUpService
 ) {
