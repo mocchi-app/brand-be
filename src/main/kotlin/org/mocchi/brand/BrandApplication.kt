27e271c40a58e1782f940636c36f37ad59674223
sausageRoll <drozdovigor1996@gmail.com>
2020-06-15 21:34:40 +0200

    Gradle fixes

--- src/main/kotlin/org/mocchi/brand/BrandApplication.kt
+++ src/main/kotlin/org/mocchi/brand/BrandApplication.kt
@@ -2,10 +2,8 @@ package org.mocchi.brand
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
 open class BrandApplication
 
 fun main(args: Array<String>) {
