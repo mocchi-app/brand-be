8932d382bad021fa2da24ea86affceebbd367887
sausageRoll <drozdovigor1996@gmail.com>
2020-08-08 11:27:22 +0200

    Refactoring

--- src/main/kotlin/org/mocchi/brand/schedule/BrandSyncJob.kt
+++ src/main/kotlin/org/mocchi/brand/schedule/BrandSyncJob.kt
@@ -1,51 +1,33 @@
 package org.mocchi.brand.schedule
 
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
-import org.mocchi.brand.convert.ShopifyProductConverter
-import org.mocchi.brand.repository.BrandProductRepository
 import org.mocchi.brand.repository.BrandRepository
-import org.mocchi.brand.service.ShopifyService
+import org.mocchi.brand.service.BrandSyncService
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.slf4j.LoggerFactory
 
 class BrandSyncJob(
-    private val brandRepository: BrandRepository,
-    private val brandProductRepository: BrandProductRepository,
-    private val shopifyProductConverter: ShopifyProductConverter,
-    private val shopifyService: ShopifyService
+        private val brandRepository: BrandRepository,
+        private val brandSyncService: BrandSyncService
 ) : Job {
     private val log = LoggerFactory.getLogger(BrandSyncJob::class.java)
 
     override fun execute(context: JobExecutionContext) {
         context.jobDetail.jobDataMap["brandId"]
-            .takeIf { it is Long }
-            ?.let { it as Long }
-            ?.let { brandId ->
-                runBlocking {
-                    brandRepository.getByIdJoinWithToken(brandId)
-                        ?.let { brand ->
-                            shopifyService.fetchAllProducts(brand.url, brand.token.token)
-                                .map { products ->
-                                    products.map {
-                                        shopifyProductConverter.convert(brandId, it)
-                                    }
+                .takeIf { it is Long }
+                ?.let { it as Long }
+                ?.let { brandId ->
+                    runBlocking {
+                        brandRepository.getByIdJoinWithToken(brandId)
+                                ?.let { brand ->
+                                    brandSyncService.syncBrandWithId(brandId, brand)
                                 }
-                                .collect { products ->
-                                    brandProductRepository.insertBrandProduct(products)
-                                        .also {
-                                            log.info("During sync for brand = $brandId ${it.count()} were synced")
-                                        }
-                                }
-                        }
+                    }
                 }
-            }
-            ?: run {
-                log.warn("Can't find brand in ${context.jobDetail.jobDataMap}")
-            }
+                ?: run {
+                    log.warn("Can't find brand in ${context.jobDetail.jobDataMap}")
+                }
 
     }
 }
